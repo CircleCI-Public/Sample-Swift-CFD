cc2ea5757f4c651d65b0f8377f80efaf54227fdd
ryanbourdais <ryanbourdais@outlook.com>
2023-04-17 15:27:27 -0500

    fixed linting

--- SampleSwiftCfd/ModelData.swift
+++ SampleSwiftCfd/ModelData.swift
@@ -8,24 +8,21 @@
 import Foundation
 import Combine
 
-final class ModelData: ObservableObject{
+final class ModelData: ObservableObject {
     @Published var foods: [Food] = load("data.json")
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-    
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
     else {
         fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
     do {
         data = try Data(contentsOf: file)
     } catch {
         fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
